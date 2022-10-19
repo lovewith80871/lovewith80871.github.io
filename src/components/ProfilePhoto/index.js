9a2c286ccab95ad49f973ddee63e4b2569061863
lovewith80871 <lovewith80871@gmail.com>
2022-10-19 17:59:56 +0800

    Deploy website - based on 2d403bfc4615330aaee0ff8812ab4c284fd92122

--- src/components/ProfilePhoto/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './styles.module.css';
-
-const ProfilePhoto = (props) => {
-  const { src } = props ?? {};
-  return <img src={src} className={styles.profile} />;
-};
-
-export default ProfilePhoto;
