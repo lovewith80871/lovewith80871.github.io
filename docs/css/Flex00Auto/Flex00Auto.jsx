43ee66bc0ba7c7912fb38c8bf3019ac86bec0635
henry_y_wu <henry_y_wu@trendmicro.com>
2023-11-19 22:03:07 +0800

    CSS Flex 0 0 auto

---
+++ docs/css/Flex00Auto/Flex00Auto.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export const Flex00Auto = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item1}>LongLongLongLongLongLongLongLong Text</div>
+      <div className={styles.item2}>Fix Width</div>
+    </div>
+  );
+};
